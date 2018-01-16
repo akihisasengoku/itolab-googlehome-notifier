96b4a4f4f441b0bd1c9c3628201cdadb60f99b5c
akihia.sengoku <akihisa1788@yahoo.co.jp>
2018-01-16 19:33:26 +0900

    up

---
+++ example.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var googlehome = require('./google-home-notifier');
+var ngrok = require('ngrok');
+var bodyParser = require('body-parser');
+var app = express();
+const serverPort = 8091; // default port
+
+var deviceName = 'Google Home';
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.post('/google-home-notifier', urlencodedParser, function (req, res) {
+
+  if (!req.body) return res.sendStatus(400)
+  if (!req.body.ip) return res.sendStatus(400)
+
+  var text = req.body.text;
+  var ip = req.body.ip;
+
+
+  var language = 'ja'; // default language code
+  if (req.query.language) {
+    language;
+  }
+
+  googlehome.ip(ip, language);
+  googlehome.device(deviceName,language)
+
+  if (text){
+    try {
+      if (text.startsWith('http')){
+        var mp3_url = text;
+        googlehome.play(mp3_url, function(notifyRes) {
+          console.log(notifyRes);
+          res.send(deviceName + ' will play sound from url: ' + mp3_url + '\n');
+        });
+      } else {
+        googlehome.notify(text, function(notifyRes) {
+          console.log(notifyRes);
+          res.send(deviceName + ' will say: ' + text + '\n');
+        });
+      }
+    } catch(err) {
+      console.log(err);
+      res.sendStatus(500);
+      res.send(err);
+    }
+  }else{
+    res.send('Please GET "text=Hello Google Home"');
+  }
+})
+
+app.get('/google-home-notifier', function (req, res) {
+
+  console.log(req.query);
+
+  var text = req.query.text;
+
+  if (req.query.ip) {
+     ip = req.query.ip;
+  }
+
+  var language = 'ja'; // default language code
+  if (req.query.language) {
+    language;
+  }
+
+  googlehome.ip(ip, language);
+  googlehome.device(deviceName,language)
+
+  if (text) {
+    try {
+      if (text.startsWith('http')){
+        var mp3_url = text;
+        googlehome.play(mp3_url, function(notifyRes) {
+          console.log(notifyRes);
+          res.send(deviceName + ' will play sound from url: ' + mp3_url + '\n');
+        });
+      } else {
+        googlehome.notify(text, function(notifyRes) {
+          console.log(notifyRes);
+          res.send(deviceName + ' will say: ' + text + '\n');
+        });
+      }
+    } catch(err) {
+      console.log(err);
+      res.sendStatus(500);
+      res.send(err);
+    }
+  }else{
+    res.send('Please GET "text=Hello+Google+Home"');
+  }
+})
+
+app.listen(serverPort, function () {
+  ngrok.connect(serverPort, function (err, url) {
+    console.log('Endpoints:');
+	  console.log("curl -X POST -d ext=Hello Google Home&ip=122.12.111 http://localhost/google-home-notifier");
+  });
+})
